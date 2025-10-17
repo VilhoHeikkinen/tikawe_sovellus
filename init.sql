c5dd9c8c51656efa4bb35ab62039c473cb1bedb7
Vilho Heikkinen <vilho.heikkinen@helsinki.fi>
2025-10-17 23:43:44 +0300

    Fixed typo in classes

--- init.sql
+++ init.sql
@@ -1,7 +1,7 @@
 DELETE FROM classes;
 
 INSERT INTO classes (title, value) VALUES ('genre', 'EDM');
-INSERT INTO classes (title, value) VALUES ('genre', 'Hip Hop/Rap');
+INSERT INTO classes (title, value) VALUES ('genre', 'Hiphop/Rap');
 INSERT INTO classes (title, value) VALUES ('genre', 'Jazz');
 INSERT INTO classes (title, value) VALUES ('genre', 'Pop');
 INSERT INTO classes (title, value) VALUES ('genre', 'Rock');
